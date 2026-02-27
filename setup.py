327f63946b662c11aea23e95c93254d49835d96a
alexgonzvalle <alexgonzvalle@gmail.com>
2026-02-27 10:45:00 +0100

    Nueva libreria (refactorizacion grande)

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     url=GITHUB_URL,
     keywords=[],
     install_requires=[
-        'requests', 'xarray', 'xmltodict', 'urllib3', 'wget', 'numpy', 'datetime', 'tqdm', 'copernicusmarine'
+        'requests', 'xarray', 'xmltodict', 'urllib3', 'numpy'
     ],
     package_data={'': ['']},
     include_package_data=True,
